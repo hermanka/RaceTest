473793e3e84e50054c49d1197fde8576962ee23b
hermanka <hermanka.beta@gmail.com>
2020-02-11 15:30:09 +0700

    add image

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function pdo_connect(){
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
-    $DATABASE_NAME = 'simplecrud';
+    $DATABASE_NAME = 'racetest';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
