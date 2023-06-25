0aaf238bd6fb6d276f26eb9538fb9f67e235645d
hermanka <hermanka.beta@gmail.com>
2023-06-25 20:59:42 +0700

    add styles

--- index.php
+++ index.php
@@ -1,15 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php
+include "functions.php";
+$pdo = pdo_connect();
+$stmt = $pdo->prepare('SELECT * FROM deposit');
+$stmt->execute();
+$deps = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    <title>race test</title>
-</head>
-<body>
-    Just another test<br>
-    example use<br>
-    read test.sh    
-</body>
-</html>
+if (isset($_GET['id'])) {
+    $stmtr = $pdo->prepare('UPDATE deposit set amount= 1000000 WHERE id = ?');
+    $stmtr->execute([$_GET['id']]);
+    header("location: index.php");
+}
+
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <?= style_script() ?>
+        <script>
+            $(document).ready(function() {
+                $('#deposit').DataTable();
+            });
+        </script>
+
+        <title>Poor Bank, inc.</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <h2>Just A Poor Bank</h2>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <table class="table table-striped" id="deposit">
+                        <thead>
+                            <tr>
+                                <th>Account ID</th>
+                                <th>Amount</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($deps as $dep) : ?>
+                                <tr>
+                                    <td><?= $dep['id'] ?></td>
+                                    <td><?= $dep['amount'] ?></td>
+                                    <td class="actions">
+                                        <a type="button" class="btn btn-outline btn-danger" href="index.php?id=<?= $dep['id'] ?>" class="trash" onclick="return confirm('Are you sure you want to reset this account?');">RESET</a>
+                                    </td>
+                                </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Account ID</th>
+                                <th>Amount</th>
+                                <th></th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <p class="mt-5 mb-3 text-muted">hk &copy; 2023</p>
+        </div>
+    </body>
+
+    </html>
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 
     #! /bin/bash
     echo "hello"
+    i = 1
     for y in `seq 1 10`
     do (for x in `seq 1 10`
     do curl -s "http://localhost/racetest/pull.php?id=1&amount=100"
