5f88dd18e51e07fd9840c1a1499f15422c21d328
hermanka <hermanka.beta@gmail.com>
2023-06-25 20:33:48 +0700

    rem

--- functions.php
+++ functions.php
@@ -12,14 +12,4 @@ function pdo_connect(){
     }
 }
 
-function style_script(){
-    return '<link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
-    <script src="js/jquery-3.4.1.slim.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>';
-}
 ?>
--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 
     #! /bin/bash
     echo "hello"
-    for y in `seq 1 8`
+    for y in `seq 1 10`
     do (for x in `seq 1 10`
-    do curl -s http://localhost/race_aut/pull/1/3000
+    do curl -s "http://localhost/racetest/pull.php?id=1&amount=100"
     done)&
     done
